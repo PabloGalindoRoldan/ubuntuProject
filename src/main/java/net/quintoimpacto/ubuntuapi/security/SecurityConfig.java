9b4f2d12472eabed79df6cc8d67c19c30ec01bd9
Alondra <alonngadea@gmail.com>
2024-08-13 11:11:25 -0300

    Feat pagination

--- src/main/java/net/quintoimpacto/ubuntuapi/security/SecurityConfig.java
+++ src/main/java/net/quintoimpacto/ubuntuapi/security/SecurityConfig.java
@@ -10,8 +10,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.google.api.client.http.HttpMethods;
-
 import net.quintoimpacto.ubuntuapi.security.jwt.JwtTokenAuthenticationFilter;
 
 @Configuration
@@ -38,13 +36,13 @@ public class SecurityConfig {
                         authorizeRequests
                                 .requestMatchers("/", "/login/oauth2/**", "/error").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/countries", "/provinces", "/images", "images/{id}", "/microbusiness/findAll", "/microbusiness/").permitAll()
-                                .requestMatchers(HttpMethod.GET,  "/questions/initial", "/questions/subquestions/{answerId}", "/answer/all").permitAll()
+                                .requestMatchers(HttpMethod.GET,  "/questions/initial", "/questions/subquestions/{answerId}", "/answer/all", "/getAllPublications", "/getAllPublications/{id}").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/contact/").permitAll()
                                 .requestMatchers("/contact/**").hasRole("ADMIN")
                                 .requestMatchers("/user").hasRole("USER")
                                 .requestMatchers("/admin").hasRole("ADMIN")
                                 .requestMatchers("/microbusiness/**").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.POST, "/questions/create", "/answer/create").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.POST, "/questions/create", "/answer/create", "/createPublication").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.PUT, "/questions/update/{id}").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                                 //.requestMatchers("/images","/**").hasRole("ADMIN")
