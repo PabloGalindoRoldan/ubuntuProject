874851e62847e6fba2f2ada8b793d874126194c1
Alondra <alonngadea@gmail.com>
2024-08-27 14:51:38 -0300

    resolve merge

--- ubuntu-frontend/src/Components/view/Searchbar/ViewSearchBar.jsx
+++ ubuntu-frontend/src/Components/view/Searchbar/ViewSearchBar.jsx
@@ -8,12 +8,12 @@ const ViewSearchBar = () => {
         sx={{
           backgroundSize: "cover",
           backgroundPosition: "center",
-          height: { xs: "300px", sm: "400px", md: "488px" }, // Ajusta la altura según el tamaño de la pantalla
+          height: { xs: "300px", sm: "400px", md: "488px" }, 
           display: "flex",
           flexDirection: "column",
           justifyContent: "flex-start",
           alignItems: "center",
-          padding: { xs: "10px 10px 0", md: "20px 20px 0" }, // Ajusta el padding para diferentes pantallas
+          padding: { xs: "10px 10px 0", md: "20px 20px 0" }, 
           position: "relative",
         }}
       >
